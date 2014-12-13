120cbcbe543cd943f1bef2c9b702cc4725cec27d
Florentin Hennecker <florentin.hennecker@gmail.com>
2014-12-13 19:01:09 +0100

    [add] writeFromJson to ObjConverter

--- src/meshing/ObjConverter.py
+++ src/meshing/ObjConverter.py
@@ -1,3 +1,5 @@
+from json import loads
+
 class ObjConverter:
 	def __init__(self, filename):
 		self.filename = filename
@@ -18,6 +20,12 @@ class ObjConverter:
 		""" Appends point x, y, z to file """
 		self.f.write("v %f %f %f\n" % (x, y, z))
 
+	def writeFromJson(self, filename):
+		self.write(loads(open(filename, 'r').read()))
+
 if __name__ == "__main__":
 	oc = ObjConverter("bonjour")
 	oc.write([[0,0,0],[1,1,1],[2,2,2], [0,0,1], [0,1,0], [1, 0,0]])
+	oc2 = ObjConverter("reducted_phone")
+	oc2.writeFromJson("reducted_phone.json")
+
