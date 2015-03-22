8502cfea39c53b9a1a01bb2ea37faab945f3ea58
MartinCornil <macornil@ulb.ac.be>
2015-03-22 12:16:28 +0100

    [add] modify hsv filter to accept high brightness (cyan) from lasers

--- src/scanner/image.py
+++ src/scanner/image.py
@@ -15,28 +15,32 @@ class ImageProcessor:
         mask = cv2.bitwise_and(foreground,foreground,mask = mask)
         return self.massCenter(mask)
 
-    def getRGBmask(self, imageDiff, R_threshold=10, GB_threshold=5):
+    def getRGBmask(self, imageDiff, R_threshold=20, GB_threshold=5):
         lower = np.array([0, 0, R_threshold], dtype=np.uint8)
         upper = np.array([GB_threshold, GB_threshold, 255], dtype=np.uint8)
-        mask = cv2.inRange(imageDiff, lower, upper)
-        return cv2.GaussianBlur(mask,(3,3),0)
+        return cv2.inRange(imageDiff, lower, upper)
 
     def getHSVmask(self, imageDiff, luminosity=20):
         image = cv2.cvtColor(imageDiff, cv2.COLOR_BGR2HSV)
 
-        lower = np.array([0, 10, luminosity], dtype=np.uint8)
+        lower = np.array([0, 170, luminosity], dtype=np.uint8)
         upper = np.array([10, 255, 255], dtype=np.uint8)
-        mask1 = cv2.inRange(image, lower, upper)
+        mask = cv2.inRange(image, lower, upper)
 
-        lower = np.array([170, 10, luminosity], dtype=np.uint8)
+        lower = np.array([170, 170, luminosity], dtype=np.uint8)
         upper = np.array([180, 255, 255], dtype=np.uint8)
-        mask2 = cv2.inRange(image, lower, upper)
+        mask = cv2.bitwise_or(cv2.inRange(image, lower, upper), mask)
 
-        return np.bitwise_or(mask1, mask2)
+        # high brightness, red is blue (cyan)
+        lower = np.array([80, 0, 200], dtype=np.uint8)
+        upper = np.array([100, 255, 255], dtype=np.uint8)
+        mask = cv2.bitwise_or(cv2.inRange(image, lower, upper), mask)
+        return mask
 
     def getLaserMask(self, foreground, background):
-        imageDiff = np.array(foreground, dtype=np.int16) - background
+        imageDiff = np.array(foreground, dtype=np.int16) - np.array(background, dtype=np.int16)
         imageDiff = np.array(imageDiff.clip(0, 255), dtype=np.uint8)
+        imageDiff = cv2.medianBlur(imageDiff,3)
         mask = cv2.bitwise_or(self.getRGBmask(imageDiff), self.getHSVmask(imageDiff))
         mask = cv2.GaussianBlur(mask,(3,3),0)
         return cv2.inRange(mask, np.array([250]), np.array([255]))
